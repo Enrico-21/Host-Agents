50b0de39bbcbac36fa4bf0eaeb1d9d81fe1277e2
Enrico-21 <enrico@hostagents.com>
2026-01-16 14:54:55 +0200

    index main
    The OG

---
+++ index.php
@@ -0,0 +1,33 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+  <meta charset="utf-8">
+  <meta http-equiv="X-UA-Compatible" content="IE=edge">
+  <meta name="viewport" content="width=device-width, initial-scale=1, minimum-scale=1, maximum-scale=1">
+  <title>Airbnb Property Management in Mauritius - HostAgents</title>
+  <meta name="description"
+    content="HostAgents is a Professional Airbnb Property Manager in Mauritius taking care of everytghing from bookings, guests, housekeeping and maintenance.">
+  <meta name="keywords"
+    content="airbnb management, airbnb management mauritius, airbnb property management mauritius, mauritius property management, airbnb mauritius property management, professional airbnb management">
+
+  <meta name="ROBOTS" content="index, follow">
+  <?php include 'includes/scripts-css.php'; ?>
+</head>
+
+<body>
+  <!-- header -->
+  <?php include 'includes/header.php'; ?>
+  <!-- /. Header -->
+
+  <section class="testimonials_footer_wrap">
+    <div class="container">
+      <img src="/images/testimonial-thumbs-01.png" alt="" title="">
+    </div>
+  </section>
+
+  <?php include 'includes/footer.php'; ?>
+
+</body>
+
+</html>
